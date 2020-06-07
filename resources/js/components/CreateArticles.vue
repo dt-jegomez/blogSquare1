8c6c04bb9f612af0432a44fe5b621c3cbf820d6c
Juan Gomez <go.juangomez23@gmail.com>
2020-06-07 09:13:10 -0500

    restructuracion blade

---
+++ resources/js/components/CreateArticles.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <h1>hola mundo</h1>
+    </section>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <div class="" id="app">
-    <example-component></example-component>
+    <create-articles />
 </div>
 @endsection
