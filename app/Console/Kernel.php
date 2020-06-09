13836fce9f6aaf55c792763205a460c000366a20
Juan Gomez <go.juangomez23@gmail.com>
2020-06-08 22:26:11 -0500

    shedule comoan kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('demo:cron')->everyFiveMinutes();
+        $schedule->command('demo:cron')->hourly();
     }
 
     /**
